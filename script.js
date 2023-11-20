a18432e476b353c38f3472ae1fc1b38d2ba2bcb5
eadanie1 <eadaniel.larsson@gmail.com>
2023-11-20 21:54:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -84,7 +84,75 @@ headerTopLinks.style.margin = '0 30px 0 0';
 
 const headerBottomLinks = document.createElement('div');
 headerBottomLinks.id = 'header-bottom-links';
-headerBottomLinks.style.margin = '0 30px 0 0';
+headerBottomLinks.style.margin = '0 25px 0 0';
+
+const linkText = document.createElement('div');
+linkText.id = 'link-text';
+linkText.style.lineHeight = '0px';
+
+const downArrowSmallIcon = document.createElement('div');
+downArrowSmallIcon.id = 'down-arrow-small-icon';
+downArrowSmallIcon.style.marginLeft = '5px';
+downArrowSmallIcon.style.width = '9px';
+
+const downArrowLargeIcon = document.createElement('div');
+downArrowLargeIcon.id = 'down-arrow-large-icon';
+downArrowLargeIcon.style.marginLeft = '5px';
+downArrowLargeIcon.style.width = '12px';
+
+const searchIcon = document.createElement('div');
+searchIcon.id = 'search-icon';
+searchIcon.style.width = '16px';
+searchIcon.style.height = '15px';
+searchIcon.style.marginRight = '5px';
+
+const searchLink = document.createElement('div');
+searchLink.id = 'search-link';
+searchLink.style.display = 'flex';
+searchLink.style.justifyContent = 'center';
+searchLink.style.alignItems = 'center';
+searchLink.style.textDecoration = 'none';
+
+const locationPinIcon = document.createElement('div');
+locationPinIcon.id = 'location-pin-icon';
+locationPinIcon.style.width = '17px';
+locationPinIcon.style.marginRight = '5px';
+
+const locationLink = document.createElement('div');
+locationLink.id = 'location-link';
+locationLink.style.color = 'rgb(0,107,174)';
+locationLink.style.display = 'flex';
+locationLink.style.justifyContent = 'center';
+locationLink.style.alignItems = 'center';
+locationLink.style.textDecoration = 'underline';
+
+const orderNowButton = document.createElement('div');
+orderNowButton.id = 'order-now-button';
+orderNowButton.style.backgroundColor = 'rgb(255, 188, 13)';
+orderNowButton.style.border = 'none';
+orderNowButton.style.borderRadius = '4px';
+orderNowButton.style.padding = '8px 25px';
+orderNowButton.style.fontSize = '16px';
+orderNowButton.style.lineHeight = '24px';
+
+const overlay = document.createElement('div');
+overlay.id = 'overlay';
+overlay.style.position = 'fixed';
+overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+overlay.style.top = '0';
+overlay.style.left = '0';
+overlay.style.right = '0';
+overlay.style.bottom = '0';
+overlay.style.display = 'flex';
+overlay.style.alignItems = 'center';
+overlay.style.justifyContent = 'center';
+overlay.style.zIndex = '-1';
+
+// probably change below to onclick JS function
+const orderNowButtonActive = document.createElement('div');
+orderNowButtonActive.id = 'order-now-button-active';
+orderNowButtonActive.style.backgroundColor = 'rgba(0,0,0,0.4)';
+orderNowButtonActive.style.zIndex = '50';
 
 // // MAIN CONTAINER AND MAIN CONTENT CONTAINER
 
@@ -101,9 +169,6 @@ mainContentContainer.style.maxWidth = '1200px';
 mainContentContainer.style.backgroundColor = 'red';
 mainContentContainer.textContent = 'red';
 mainContainer.appendChild(mainContentContainer);
-
-
-// HEADING
 
 // mainContainer.appendChild(headingContainer);
 const headingContainer = document.createElement('div');
